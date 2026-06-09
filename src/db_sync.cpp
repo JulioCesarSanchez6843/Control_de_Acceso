29f9a0d20932db84e550f01f7dc242a42c889b49
César <sanmenjc45@gmail.com>
2026-06-09 17:49:16 -0600

    correcion final

--- src/db_sync.cpp
+++ src/db_sync.cpp
@@ -8,9 +8,7 @@
 static const char* SERVER_URL = "http://192.168.100.11:8000";
 static const uint32_t HTTP_TIMEOUT_MS = 15000;
 
-// --------------------------------------------------
-// Utilidades
-// --------------------------------------------------
+
 static bool wifiReady() {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println("DB_SYNC: sin WiFi");
